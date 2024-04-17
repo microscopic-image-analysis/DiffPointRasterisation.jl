310c88dd9ea585db695a24d5e940f6a40bd65d2f
Wolfhart Feldmeier <wolfhart.feldmeier@uni-jena.de>
2024-04-17 15:05:08 +0200

    support point-individual weights

--- test/cuda.jl
+++ test/cuda.jl
@@ -6,10 +6,16 @@
     include("util.jl")
     cuda_available = CUDA.functional()
     
-    args = (D.grid_size_3d, D.more_points, D.rotations_static, D.translations_3d_static, D.backgrounds, D.weights)
+    # no projection
+    args = (D.grid_size_3d, D.more_points, D.rotations_static, D.translations_3d_static, D.backgrounds, D.weights, D.more_point_weights)
     @test cuda_cpu_agree(raster, args...) skip=!cuda_available
 
-    args = (D.grid_size_2d, D.more_points, D.projections_static, D.translations_2d_static, D.backgrounds, D.weights)
+    # default arguments
+    args = (D.grid_size_3d, D.more_points, D.rotations_static, D.translations_3d_static)
+    @test cuda_cpu_agree(raster, args...) skip=!cuda_available
+
+    # projection
+    args = (D.grid_size_2d, D.more_points, D.projections_static, D.translations_2d_static, D.backgrounds, D.weights, D.more_point_weights)
     @test cuda_cpu_agree(raster, args...) skip=!cuda_available
 end
 
@@ -20,12 +26,18 @@ end
     include("util.jl")
     cuda_available = CUDA.functional()
 
+    # no projection
     ds_dout_3d = randn(D.grid_size_3d..., D.batch_size)
-    args = (ds_dout_3d, D.more_points, D.rotations_static, D.translations_3d_static, D.backgrounds, D.weights)
+    args = (ds_dout_3d, D.more_points, D.rotations_static, D.translations_3d_static, D.backgrounds, D.weights, D.more_point_weights)
     @test cuda_cpu_agree(raster_pullback!, args...) skip=!cuda_available
 
+    # default arguments
+    args = (ds_dout_3d, D.more_points, D.rotations_static, D.translations_3d_static)
+    @test cuda_cpu_agree(raster_pullback!, args...) skip=!cuda_available
+
+    # projection
     ds_dout_2d = randn(D.grid_size_2d..., D.batch_size)
-    args = (ds_dout_2d, D.more_points, D.projections_static, D.translations_2d_static, D.backgrounds, D.weights)
+    args = (ds_dout_2d, D.more_points, D.projections_static, D.translations_2d_static, D.backgrounds, D.weights, D.more_point_weights)
     @test cuda_cpu_agree(raster_pullback!, args...) skip=!cuda_available
 end
 
