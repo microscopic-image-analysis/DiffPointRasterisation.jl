92e6daa4c41ef7fd36f05a8638759901981d1d10
Wolfhart Feldmeier <wolfhart.feldmeier@uni-jena.de>
2024-05-06 11:03:51 +0200

    Aqua

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,8 @@
 using TestItemRunner: @run_package_tests
 
+@testitem "Aqua.test_all" begin
+    import Aqua
+    Aqua.test_all(DiffPointRasterisation)
+end
+
 @run_package_tests # filter=ti-> occursin("CUDA", ti.name)
